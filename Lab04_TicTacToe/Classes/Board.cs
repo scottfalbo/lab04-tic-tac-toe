0e6aa674c769aedefd80350ffeb993ec0b768c3f
Scott Falbo <scottfalboart@gmail.com>
2021-01-14 17:46:49 -0800

    End of lab commit.

---
+++ Lab04_TicTacToe/Classes/Board.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Lab04_TicTacToe.Classes
+{
+    class Board
+    {
+		/// <summary>
+		/// Tic Tac Toe Gameboard states
+		/// </summary>
+		public static string[,] GameBoard = new string[,]
+		{
+			{"1", "2", "3"},
+			{"4", "5", "6"},
+			{"7", "8", "9"},
+		};
+
+
+		public static void DisplayBoard()
+		{
+
+            //TODO: Output the board to the console
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+					string row = "|" + GameBoard[i, j] + "|";
+				Console.Write(row);
+
+                }
+				Console.WriteLine("\n");
+            }
+		
+		}
+	}
+}
+
+// string newRow = string.Join(" | ", GameBoard[i]);
---
+++ Lab04_TicTacToe/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Lab04_TicTacToe.Classes;
+
+namespace Lab04_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Board.DisplayBoard();
+            StartGame();
+        }
+
+        static void StartGame()
+        {
+            // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+            // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
+            Player playerOne = CreatePlayer();
+            Player playerTwo = CreatePlayer();
+            playerOne.IsTurn = true;
+            Game newGame = MakeGame(playerOne, playerTwo);
+
+            Player winner = newGame.Play(playerOne, playerTwo);
+
+        }
+        public static Player CreatePlayer()
+        {
+            Console.WriteLine("Player one please enter your name:");
+            string userInput = Console.ReadLine();
+            Player newPlayer = new Player()
+            {
+                Name = userInput
+            };
+            return newPlayer;
+        }
+        public static Game MakeGame(Player playerOne, Player playerTwo)
+        {
+            return new Game(playerOne, playerTwo);
+        }
+
+    }
+}
+/*
+      Car johnsThirdCar = new Car()
+      {
+        Make = "Toyota Tundra",
+        Color = "Red"
+      };
+*/
